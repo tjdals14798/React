f96e968f4ab8c2f7749db690a39441affaba3860
성민 <tjdals14798@naver.com>
2022-05-10 20:52:05 +0900

    -22-05-10

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import AppReducer from './AppReducer';
+//import AppReducer from './AppReducer';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
